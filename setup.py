e6810dce44f1c3e7dadc3f200c434b9a21d77082
miedzinski <dominik@mdzn.pl>
2015-12-05 13:21:29 +0100

    Change query in show

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='worktime-logger',
-    version='0.1',
+    version='0.2',
     description='Keep track of your daily worktime.',
     long_description='Please see the `README <https://github.com/miedzinski/worktime/blob/master/README.md>`_ for a full description.',
     author='mdzn',
